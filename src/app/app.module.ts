0075827020a22dfc630ca2ee30f29e17d3d0b763
Shubham Shukla <shubham.shukla@blackrock.com>
2022-09-24 22:41:10 -0400

    Section 3 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopiingEditComponent } from './shopping-list/shopiing-edit/shopiing-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopiingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/shopiing-edit/shopiing-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopiing-edit',
+  templateUrl: './shopiing-edit.component.html',
+  styleUrls: ['./shopiing-edit.component.css']
+})
+export class ShopiingEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
